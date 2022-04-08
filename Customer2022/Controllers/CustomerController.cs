331f2b438260c33b1d2afecdc1f1ea6238d81374
Tebello Keneth Nhlapo <NhlapoT@timesmedia.co.za>
2022-04-08 16:56:21 +0200

    first commit from local machine

---
+++ Customer2022/Controllers/CustomerController.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using Customer2022.Models;
+
+namespace Customer2022.Controllers
+{
+    public class CustomerController : Controller
+    {
+        string connectionString = @"Data Source=.\SQLEXPRESS; Database=ASPCRUD;Integrated Security=True";
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            DataTable dtblCustomer = new DataTable();
+            using (SqlConnection sqlcon = new SqlConnection(connectionString))
+            {
+                sqlcon.Open();
+                SqlDataAdapter sqlDa = new SqlDataAdapter("SELECT * from Contact", sqlcon);
+                sqlDa.Fill(dtblCustomer);
+            }
+            return View(dtblCustomer);
+        }
+
+
+
+        // GET: Customer/Create
+        public ActionResult Create()
+        {
+            return View(new Customers());
+        }
+
+        // POST: Customer/Create
+        [HttpPost]
+        public ActionResult Create(Customers customers)
+        {
+
+            // TODO: Add insert logic here
+            using (SqlConnection sqlcon = new SqlConnection(connectionString))
+            {
+                sqlcon.Open();
+                string query = " INSERT INTO Contact VALUES(@Name,@Mobile,@Address)";
+                SqlCommand sqlcmd = new SqlCommand(query, sqlcon);
+                sqlcmd.Parameters.AddWithValue("@Name", customers.Name);
+                sqlcmd.Parameters.AddWithValue("@Mobile", customers.Mobile);
+                sqlcmd.Parameters.AddWithValue("@Address", customers.Address);
+                sqlcmd.ExecuteNonQuery();
+            }
+            return RedirectToAction("Index");
+
+
+        }
+
+        // GET: Customer/Edit/5
+        public ActionResult Edit(int id) { 
+         Customers customermodel = new Customers();
+        DataTable dtblcus = new DataTable();
+        using(SqlConnection sqlcon = new SqlConnection(connectionString)){
+                sqlcon.Open();
+                string query = "SELECT * From Contact Where ContactID = @ContactID";
+                SqlDataAdapter sqlDa = new SqlDataAdapter(query,sqlcon);
+                sqlDa.SelectCommand.Parameters.AddWithValue("@ContactID",id);
+                sqlDa.Fill(dtblcus);
+        }
+            if(dtblcus.Rows.Count == 1)
+            {
+                customermodel.ContactID = Convert.ToInt32(dtblcus.Rows[0][0].ToString());
+                customermodel.Name = dtblcus.Rows[0][1].ToString();
+                customermodel.Mobile = dtblcus.Rows[0][2].ToString();
+                customermodel.Address = dtblcus.Rows[0][3].ToString();
+                return View(customermodel);
+            }
+            else
+                  return RedirectToAction("Index");
+        }
+
+        // POST: Customer/Edit/5
+        [HttpPost]
+        public ActionResult Edit(Customers customers)
+        {
+            using (SqlConnection sqlcon = new SqlConnection(connectionString))
+            {
+                sqlcon.Open();
+                string query = "UPDATE Contact SET Name = @Name, Mobile = @Mobile, Address = @Address WHere ContactID = @ContactID";
+                SqlCommand sqlcmd = new SqlCommand(query, sqlcon);
+                sqlcmd.Parameters.AddWithValue("@ContactID", customers.ContactID);
+                sqlcmd.Parameters.AddWithValue("@Name", customers.Name);
+                sqlcmd.Parameters.AddWithValue("@Mobile", customers.Mobile);
+                sqlcmd.Parameters.AddWithValue("@Address", customers.Address);
+                sqlcmd.ExecuteNonQuery();
+            }
+            return RedirectToAction("Index");
+        }
+
+        // GET: Customer/Delete/5
+        public ActionResult Delete(int id)
+        {
+            using (SqlConnection sqlcon = new SqlConnection(connectionString))
+            {
+                sqlcon.Open();
+                string query = "DELETE From Contact  WHere ContactID = @ContactID";
+                SqlCommand sqlcmd = new SqlCommand(query, sqlcon);
+                sqlcmd.Parameters.AddWithValue("@ContactID",id);
+              
+                sqlcmd.ExecuteNonQuery();
+            }
+            return RedirectToAction("Index");
+        }
+
+        
+    }
+}
---
+++ Customer2022/Models/Customers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace Customer2022.Models
+{
+    public class Customers
+    {
+        public int ContactID { get; set; }
+        [DisplayName("Customer Name")]
+        public string Name { get; set; }
+        public string Mobile { get; set; }
+        public string  Address  { get; set; }
+    }
+}
